1d94fed2f5b22c6129365f54887632df3921af12
pycodebr <pycodebr@gmail.com>
2021-02-13 14:34:02 -0300

    first commit

---
+++ bar-code-gen.py
@@ -0,0 +1,6 @@
+from barcode import EAN13
+from barcode.writer import ImageWriter
+
+# Gera o código de barras e salva no caminho desejado #
+with open(r'D:\pycode\bar-code-generator\barcode.png', 'wb') as f:
+    EAN13('123456789102', writer=ImageWriter()).write(f)
